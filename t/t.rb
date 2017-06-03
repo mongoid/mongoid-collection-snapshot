2212132aa41218e30f0ba3433d73dc78b7a2e172
dblock <dblock@dblock.org>
2017-06-03 11:08:13 -0400

    Consistent repro, takes 3 iterations to get back to a snapshot without a number.

--- t/t.rb
+++ t/t.rb
@@ -23,10 +23,9 @@ class WidgetsAndGadgets
   end
 
   def build
-    puts "#{collection_snapshot.name} in #{collection_snapshot.database.name} has #{collection_snapshot.count} items"
     Widget.all.each do |widget|
       Gadget.all.each do |gadget|
-        puts "inserted #{widget.id} x #{gadget.id}"
+        puts "inserted #{widget.id} x #{gadget.id} into #{collection_snapshot.name}"
         collection_snapshot.insert_one(widget_id: widget.id, gadget_id: gadget.id)
       end
     end
@@ -47,14 +46,20 @@ puts "created gadget #{g._id}"
 
 WidgetsAndGadgets.each.map(&:collection_snapshot).map(&:drop)
 
-WidgetsAndGadgets.create!
+3.times do
+  WidgetsAndGadgets.create!
+end
 
 require 'thread'
 
 workers = (0...8).map do
   Thread.new do
     WidgetsAndGadgets.latest.documents.each do |pair|
-      raise "got a nil widget in #{pair.widget_id} x #{pair.gadget_id}" unless pair.widget || pair.gadget
+      if pair.widget && pair.gadget
+        puts "got a widget and gadget in #{pair.widget_id} x #{pair.gadget_id}"
+      else
+        raise "got a nil widget in #{pair.widget_id} x #{pair.gadget_id}"
+      end
     end
   end
 end
